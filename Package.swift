88eaac87e739e91e2fe82bb7a6a9a7f4099c16e5
Meleshkevich Aleksey <meleshhhkevich@icloud.com>
2024-11-27 00:56:00 +0100

    Added Bazel and SPM files to gitignore

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "snapkit_example",
+    name: "FoodNotes",
     dependencies: [
         .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.4.2"),
         .package(url: "https://github.com/exyte/PopupView", from: "3.0.0"),
